39f0c13f80d507fe6d9a8e4ea468dcb8e19965f1
Lars Brandt <zebrajaeger@zebrajaeger.de>
2021-05-03 22:19:31 +0200

    cleanup

--- server/src/main/java/de/zebrajaeger/stomptest/MessageHandlerController.java
+++ server/src/main/java/de/zebrajaeger/stomptest/MessageHandlerController.java
@@ -8,34 +8,21 @@ import org.springframework.stereotype.Controller;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.util.concurrent.atomic.AtomicLong;
 
 @Controller
 public class MessageHandlerController {
 
     private final SimpMessagingTemplate template;
-    private AtomicLong counter = new AtomicLong(0);
 
     @Autowired
     public MessageHandlerController(SimpMessagingTemplate template) {
         this.template = template;
     }
 
-//    @MessageMapping("/xxx")
-//    public void send(Message message) {
-//        template.convertAndSend("/topic/messages", buildNextMessage());
-//    }
-
     @MessageMapping("/xxx")
     @SendTo("/topic/messages")
     public OutputMessage send(Message message) {
         String time = new SimpleDateFormat("HH:mm").format(new Date());
         return new OutputMessage(message.getFrom(), message.getText(), time);
-    }
-
-    private String buildNextMessage() {
-        var message = "Test" + counter.getAndIncrement();
-        System.out.println("Send message " + message);
-        return message;
     }
 }
