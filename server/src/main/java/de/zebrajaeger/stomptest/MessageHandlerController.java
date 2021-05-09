96c578537ded08d847cc63a2989af93c246fe559
Lars Brandt <zebrajaeger@zebrajaeger.de>
2021-05-09 08:09:14 +0200

    mostly working...

--- server/src/main/java/de/zebrajaeger/stomptest/MessageHandlerController.java
+++ server/src/main/java/de/zebrajaeger/stomptest/MessageHandlerController.java
@@ -1,13 +1,16 @@
 package de.zebrajaeger.stomptest;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Controller;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.HashMap;
 
 @Controller
 public class MessageHandlerController {
@@ -19,10 +22,42 @@ public class MessageHandlerController {
         this.template = template;
     }
 
-    @MessageMapping("/xxx")
-    @SendTo("/topic/messages")
-    public OutputMessage send(Message message) {
+    @MessageMapping("/rpc")
+    public void rpcCall(Message message, @Header("correlation-id") String id, @Header("reply-to") String destination) {
+        System.out.println("SEND");
+        System.out.println(id);
+        System.out.println(destination);
+
         String time = new SimpleDateFormat("HH:mm").format(new Date());
-        return new OutputMessage(message.getFrom(), message.getText(), time);
+        OutputMessage outputMessage = new OutputMessage(message.getFrom(), message.getText(), time);
+
+        HashMap<String, Object> header = new HashMap<>();
+        header.put("correlation-id", id);
+        template.convertAndSend(destination, outputMessage, header);
+    }
+
+    @MessageMapping("/from-client")
+    public void fromClient(Message message) {
+        System.out.println("FROM CLIENT");
+        System.out.println("  " + message);
+    }
+
+    @Scheduled(fixedRate = 1000)
+    public void toClient() {
+        System.out.println("TO-CLIENT(1)");
+        String time = new SimpleDateFormat("HH:mm").format(new Date());
+        OutputMessage message = new OutputMessage("to-client(1)", "ping", time);
+        template.convertAndSend("/topic/to-client", message);
+    }
+
+    /**
+     * Does not work for some reason :/
+     */
+    @Scheduled(fixedRate = 1000)
+    @SendTo("/to-client")
+    public OutputMessage toClient2() {
+        System.out.println("TO-CLIENT(2)");
+        String time = new SimpleDateFormat("HH:mm").format(new Date());
+        return new OutputMessage("to-client(2)", "ping", time);
     }
 }
--- server/src/main/java/de/zebrajaeger/stomptest/WebSocketConfig.java
+++ server/src/main/java/de/zebrajaeger/stomptest/WebSocketConfig.java
@@ -2,12 +2,14 @@ package de.zebrajaeger.stomptest;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
 @Configuration
 @EnableWebSocketMessageBroker
+@EnableScheduling
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
@@ -19,5 +21,8 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/chat").setAllowedOrigins("*");
+        registry.addEndpoint("/rpc-replies").setAllowedOrigins("*");
+        registry.addEndpoint("/to-client").setAllowedOrigins("*");
+        registry.addEndpoint("/from-client").setAllowedOrigins("*");
     }
 }
